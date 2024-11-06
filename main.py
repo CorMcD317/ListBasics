12210aa7449da1096a186edb8d4ecc19db67526d
Corbin McDougal <mcdoucor000@warren.k12.in.us>
2024-11-06 08:26:48 -0500

    Commit

---
+++ main.py
@@ -0,0 +1,16 @@
+color_list = ["red", "green", "blue", "yellow"]
+print(color_list[0])
+print(color_list[3])
+
+num_list = [4, 6, 4, 2, 9, 4, 1]
+num_list.append(5)
+print(num_list)
+num_list.remove(4)
+print(num_list)
+print(num_list.count(4))
+print(num_list.index(9))
+
+fruit_list = ["apple", "banana", "cherry"]
+fruit_list[1] = "orange"
+fruit_list.remove("cherry")
+print(fruit_list)
